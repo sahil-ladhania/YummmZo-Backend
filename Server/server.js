412c7e3dd36ae2643fcfca6624a683e7c0bf16ce
Sahil <sahilladhania5@gmail.com>
2023-06-14 15:37:22 +0530

    Implemented User Registration Authentication.

--- Server/server.js
+++ Server/server.js
@@ -9,6 +9,9 @@ const app = express();
 // Defining Port.
 const port = 81;
 
+// Adding Middlewares.
+app.use(express.json());
+
 // Conecting to Database.
 dbConnection();
 
@@ -18,12 +21,51 @@ app.get('/', (req, res) => {
 });
 
 // Defining Authentication Routes.
+// API for User Registration.
 app.post('/api/register', (req, res) => {
-    res.send('User Registration');
+    // -----Retrieving Values Subbmitted By User-----
+    const { firstName , lastName ,email , password , confirmPassword } = req.body;
+    // Validation Part.
+
+    // -----Checking if all the required Data Feilds are Filled or Not-----
+    if ( !firstName || !lastName || !email || !password || !confirmPassword ){
+        return res.status(400).json({Error : "Please Fill The Required Feilds !!!"});
+    }
+
+    // -----Checking if the Email Address is Unique or Not-----
+    User.findOne({ email : email })
+        .then((userExist) => {
+            if ( userExist ){
+                return res.status(201).json({Error : "Email Already Exist !!!"});
+            }
+            // -----Checking if the Password is same as Confirm Password or Not-----
+            else if( password != confirmPassword ){
+                return res.status(201).json({Error : "Passwords Do Not Match !!!"});
+            }
+            else{
+                const user = new User({ firstName , lastName , email , password , confirmPassword }); // Created a new instance of User model or class.
+                // Middleware Used to Hash Password.
+                user.save()
+                    .then(() => {
+                        return res.status(201).json({Message : "User Successfully Registered ..."});
+                    })
+                    .catch((error) => {
+                        return res.status(500).json({Error : error});
+                    });
+            };
+        });
 });
+
+
+
+// API for User Login.
 app.post('/api/login', (req, res) => {
     res.send('User Login');
 });
+
+
+
+// API for User Logout.
 app.post('/api/logout', (req, res) => {
     res.send('User Logout');
 });
