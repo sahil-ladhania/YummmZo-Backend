27591834e8362707d82cf1255fdcd01886ee0a0f
Sahil <sahilladhania5@gmail.com>
2023-07-03 02:48:41 +0530

    Implemented User Registration in Backend.

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ import User from '../App/Models/userAuth.js';
 import Cuisine from '../App/Models/cuisineSchema.js';
 import Restaurant from '../App/Models/restaurantSchema.js';
 import MenuItem from '../App/Models/menuItemSchema.js';
+import bcrypt from 'bcrypt';
 
 
 // Creating an Express App.
@@ -26,8 +27,38 @@ app.get('/', (req, res) => {
 
 // Defining Authentication Routes.
 // API for User Registration.
-app.post('/api/register', (req, res) => {
-    res.send('User Register');
+app.post('/api/users/register', (req, res) => {
+    // Checking For Required Feilds.
+    const { firstName , lastName , email , password , confirmPassword } = req.body;
+    // Validating Input Feilds.
+    if ( !firstName || !lastName || !email || !password || !confirmPassword ){
+        return res.status(400).send({ Error : "Please Fill The Required Feilds !!!"});
+    }
+    else{
+        // Checking For Existing Users.
+        const { email } = req.body;
+        User.findOne({ email : email })
+            .then((existingUser) => {
+                if(existingUser){
+                    return res.status(400).send({ Error : "Email Already Registered !!!"});
+                }
+                else{
+                    // Creating New Instance To The Database.
+                    const newUser = new User({ firstName , lastName , email , password , confirmPassword });
+                    // Saving The User Instance To The Database.
+                    newUser.save()
+                        .then(() => {
+                            return res.status(200).send({ Message : "User Successfully Registered ..."});
+                        })
+                        .catch((error) => {
+                            return res.status(500).send({ Error : `Error Occured While Saving The User Data. ${error}`});
+                        });
+                    }
+            })
+            .catch((error) => {
+                return res.status(500).send({ Error : `Error Occured While Saving The User Data. ${error}`});
+            })
+        }
 });
 
 // API for User Login.
