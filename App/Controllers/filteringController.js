f948db3acf81f5ef181c341cbbb413f74ed3bbe8
Sahil <sahilladhania5@gmail.com>
2023-09-27 03:35:46 +0530

    Done Some Changes.

--- App/Controllers/filteringController.js
+++ App/Controllers/filteringController.js
@@ -4,54 +4,54 @@ import Restaurant from "../Models/restaurantSchema";
 // Controller function to handle sorting and filtering.
 // -----Controller Function For Sorting Restaurants That Has Fastest Delivery.-----
 export const sortByFastDelivery = (req , res) => {
-    // Step 1: Extract query parameters from req.query.
-    
-    // Step 2: Construct the initial database query.
-    
-    // Step 3: Apply filtering criteria for fast delivery if specified.
-    // Substep 3.1: Implement filtering logic here.
-    
-    // Step 4: Execute the query and handle the results.
-    // Substep 4.1: Handle successful query execution.
-    // Substep 4.2: Implement response logic here to send the filtered data.
-    // Substep 4.3: Implement error handling logic in case of query execution failure.
+    // 1. Extract query parameters for filtering (e.g., fastDelivery) from req.query.
+    // 2. Create an initial database query to fetch restaurants.
+    // 3. Apply filtering criteria for fast delivery if specified.
+    //    - Substep 3.1: Implement filtering logic here.
+    // 4. Execute the query to retrieve filtered restaurants.
+    // 5. Handle successful query execution.
+    //    - Substep 4.1: Handle successful query execution.
+    // 6. Implement response logic to send the filtered data.
+    //    - Substep 4.2: Implement response logic here to send the filtered data.
+    // 7. Implement error handling logic in case of query execution failure.
+    //    - Substep 4.3: Implement error handling logic in case of query execution failure.
 };
 // -----Controller Function For Sorting Restaurants That Has Rating above 4.0.-----
 export const sortByRating = (req , res) => {
-    // Step 1: Extract query parameters from req.query.
-    
-    // Step 2: Construct the initial database query.
-    
-    // Step 3: Apply sorting criteria for rating (descending order) if specified.
-    
-    // Step 4: Execute the query and handle the results.
-    // Substep 4.1: Handle successful query execution.
-    // Substep 4.2: Implement response logic here to send the sorted data.
-    // Substep 4.3: Implement error handling logic in case of query execution failure.
+    // 1. Extract query parameters for sorting (e.g., rating) from req.query.
+    // 2. Create an initial database query to fetch restaurants.
+    // 3. Apply sorting criteria for rating (descending order) if specified.
+    // 4. Execute the query to retrieve sorted restaurants.
+    // 5. Handle successful query execution.
+    //    - Substep 4.1: Handle successful query execution.
+    // 6. Implement response logic to send the sorted data.
+    //    - Substep 4.2: Implement response logic here to send the sorted data.
+    // 7. Implement error handling logic in case of query execution failure.
+    //    - Substep 4.3: Implement error handling logic in case of query execution failure.
 };
 // -----Controller Function For Sorting Restaurants From Low To High Price.-----
 export const sortByCostLowToHigh = (req , res) => {
-    // Step 1: Extract query parameters from req.query.
-    
-    // Step 2: Construct the initial database query.
-    
-    // Step 3: Apply sorting criteria for cost (ascending order) if specified.
-    
-    // Step 4: Execute the query and handle the results.
-    // Substep 4.1: Handle successful query execution.
-    // Substep 4.2: Implement response logic here to send the sorted data.
-    // Substep 4.3: Implement error handling logic in case of query execution failure.
+    // 1. Extract query parameters for sorting (e.g., cost) from req.query.
+    // 2. Create an initial database query to fetch restaurants.
+    // 3. Apply sorting criteria for cost (ascending order) if specified.
+    // 4. Execute the query to retrieve sorted restaurants.
+    // 5. Handle successful query execution.
+    //    - Substep 4.1: Handle successful query execution.
+    // 6. Implement response logic to send the sorted data.
+    //    - Substep 4.2: Implement response logic here to send the sorted data.
+    // 7. Implement error handling logic in case of query execution failure.
+    //    - Substep 4.3: Implement error handling logic in case of query execution failure.
 };
 // -----Controller Function For Sorting Restaurants From High To Low Price.-----
 export const sortByCostHighToLow = (req , res) => {
-    // Step 1: Extract query parameters from req.query.
-    
-    // Step 2: Construct the initial database query.
-    
-    // Step 3: Apply sorting criteria for cost (descending order) if specified.
-    
-    // Step 4: Execute the query and handle the results.
-    // Substep 4.1: Handle successful query execution.
-    // Substep 4.2: Implement response logic here to send the sorted data.
-    // Substep 4.3: Implement error handling logic in case of query execution failure.
+    // 1. Extract query parameters for sorting (e.g., cost) from req.query.
+    // 2. Create an initial database query to fetch restaurants.
+    // 3. Apply sorting criteria for cost (descending order) if specified.
+    // 4. Execute the query to retrieve sorted restaurants.
+    // 5. Handle successful query execution.
+    //    - Substep 4.1: Handle successful query execution.
+    // 6. Implement response logic to send the sorted data.
+    //    - Substep 4.2: Implement response logic here to send the sorted data.
+    // 7. Implement error handling logic in case of query execution failure.
+    //    - Substep 4.3: Implement error handling logic in case of query execution failure.
 };
