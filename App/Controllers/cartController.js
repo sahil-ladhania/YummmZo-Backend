c3fc30b533bd579c930735ec8ce1977d00769a69
Sahil <sahilladhania5@gmail.com>
2023-09-30 06:29:40 +0530

    Deleted Cart Code.

--- App/Controllers/cartController.js
+++ App/Controllers/cartController.js
@@ -6,97 +6,17 @@ import User from "../Models/userAuth.js";
 // -----To Add An Item To The Cart.-----
 export const addItem = (req, res) => {
     // Extracting User ID, Restaurant ID, and Item ID from Request Parameters.
-    const { userId, restaurantId, menuItemId } = req.params;
     // Extracting the Form Data From The Request.
-    const { itemName, itemQuantity, itemPrice } = req.body;
     // Checking If User Has Filled The Required Details.
-    if (!itemName || !itemQuantity || !itemPrice) {
-        return res.status(400).send({ Error: "Please Fill The Required Fields!!!" });
-    } else {
-        // Find The Specific User By Its ID.
-        User.findById(userId)
-            .then((user) => {
-                if (!user) {
-                    return res.status(404).send({ Error: "User Not Found!!!" });
-                } 
-                else {
-                    // Find The User's Cart By The userId And Populate The menuItemId Field.
-                    Cart.findOne({ userId })
-                        .populate("cartItems.menuItemId")
-                        .then((usersCart) => {
-                            if (!usersCart) {
-                                // If The Cart Does Not Exist, Create A New Cart And Add The Item.
-                                const newCartItem = {
-                                    menuItemId,
-                                    restaurantId,
-                                    itemName,
-                                    itemQuantity,
-                                    itemPrice,
-                                    totalPrice: itemQuantity * itemPrice,
-                                };
-                                const newCart = new Cart({
-                                    userId,
-                                    cartItems: [newCartItem],
-                                    itemsTotal: newCartItem.totalPrice,
-                                });
-                                newCart
-                                    .save()
-                                    .then((savedCart) => {
-                                        return res.status(200).send(savedCart);
-                                    })
-                                    .catch((error) => {
-                                        return res.status(500).send({ Error: `Error Occurred While Saving The Cart: ${error}` });
-                                    });
-                            } else {
-                                // Check If The Item Already Exists In The Cart.
-                                const existingCartItem = usersCart.cartItems.find(
-                                    (item) =>
-                                        item.menuItemId._id.toString() === menuItemId &&
-                                        item.restaurantId.toString() === restaurantId
-                                );
-                                if (existingCartItem) {
-                                    // If The Item Exists, Update Its Quantity And Total Price.
-                                    existingCartItem.itemQuantity += itemQuantity;
-                                    existingCartItem.totalPrice = existingCartItem.itemQuantity * itemPrice;
-                                } 
-                                else {
-                                    // If The Item Does Not Exist, Create A New Cart Item.
-                                    const newCartItem = {
-                                        menuItemId,
-                                        restaurantId,
-                                        itemName,
-                                        itemQuantity,
-                                        itemPrice,
-                                        totalPrice: itemQuantity * itemPrice,
-                                    };
-                                    // Add the new item to the user's cartItems array.
-                                    usersCart.cartItems.push(newCartItem);
-                                }
-                                // Calculate the itemsTotal in the cart by summing up the totalPrice of all items.
-                                usersCart.itemsTotal = usersCart.cartItems.reduce(
-                                    (total, item) => total + item.totalPrice,
-                                    0
-                                );
-                                // Save the updated cart to the database.
-                                usersCart
-                                    .save()
-                                    .then((savedCart) => {
-                                        return res.status(200).send(savedCart);
-                                    })
-                                    .catch((error) => {
-                                        return res.status(500).send({ Error: `Error Occurred While Saving The Cart: ${error}` });
-                                    });
-                            }
-                        })
-                        .catch((error) => {
-                            return res.status(500).send({ Error: `Error Occurred While Finding The User's Cart: ${error}` });
-                        });
-                }
-            })
-            .catch((error) => {
-                return res.status(500).send({ Error: `Error Occurred While Finding The User: ${error}` });
-            });
-    }
+    // Find The Specific User By Its ID.
+    // Find The User's Cart By The userId And Populate The menuItemId Field.
+    // If The Cart Does Not Exist, Create A New Cart And Add The Item.
+    // Check If The Item Already Exists In The Cart.
+    // If The Item Exists, Update Its Quantity And Total Price.
+    // If The Item Does Not Exist, Create A New Cart Item.
+    // Add the new item to the user's cartItems array.
+    // Calculate the itemsTotal in the cart by summing up the totalPrice of all items.
+    // Save the updated cart to the database.
 };
 
 // -----To Increment Item In The Cart.-----
@@ -122,19 +42,5 @@ export const decrementQuantity = (req , res) => {
 // -----To Fetch All Cart Items.----
 export const fetchCartItems = (req , res) => {
     // Receive Parameters.
-    const { userId } = req.params;
     // Use the Cart Model To Find Cart Items.
-    Cart.findOne({userId})
-        .then((user) => {
-            if(!user){
-                return res.status(404).send({ Error: 'User Not Found !!!' });
-            }
-            else{
-                const cartItems = user.cartItems;
-                return res.status(200).send({ Message: "Cart Items For User", cartItems });
-            }
-        })
-        .catch((error) => {
-            return res.status(404).send({ Error: `User Not Found !!! : ${error}` });
-        })
 }
